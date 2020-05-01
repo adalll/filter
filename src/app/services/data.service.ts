57a29813ad222d8136cff080a66488588a2f422e
adalll <adalll@mail.ru>
2020-05-01 22:00:17 +0800

    Filter v.0.2 Behavior Subject Implementation

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,12 +3,45 @@ import { BehaviorSubject } from 'rxjs';
 
 import { Item } from '../models/item.model';
 import { items } from '../data/items.data';
+import {map} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  constructor (private route: ActivatedRoute,){}
+
   items$ = new BehaviorSubject<Item[]>(items);
+  public filteredItems$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.items$.value);
+
+  public getFilteredItems() {
+
+  this.route.queryParams
+    .subscribe((params) => {
+      this.items$
+        .pipe(
+          map ((items) => {
+            if (!params.name.trim() && !params.type.trim()) {
+              return items;
+            }
+            return items.filter((item) => (
+              item.name.toUpperCase().includes(
+                params.name.toUpperCase()
+              )
+              && item.type.toUpperCase().includes(
+                params.type.toUpperCase()
+              )
+            ))
+          })
+        )
+        .subscribe((items) => {
+          console.log('running...');
+          console.log(items);
+          this.filteredItems$.next(items)
+        });
+    });
+}
 
 }
--- src/app/views/result-component/result.component.ts
+++ src/app/views/result-component/result.component.ts
@@ -12,39 +12,13 @@ import { map } from 'rxjs/operators';
 })
 export class ResultComponent implements OnInit, OnDestroy {
 
-  constructor( private route: ActivatedRoute, private itemsDataService: DataService) { }
-
-  public filteredItems$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.itemsDataService.items$.value);
-  private filterSub: Subscription;
+  constructor( private itemsDataService: DataService) { }
 
   ngOnInit(): void {
-
-    this.filterSub = this.route.queryParams
-      .subscribe((params) => {
-        this.itemsDataService.items$
-          .pipe(
-            map ((items) => {
-              if (!params.name.trim() && !params.type.trim()) {
-                return items;
-              }
-              return items.filter((item) => (
-                item.name.toUpperCase().includes(
-                  params.name.toUpperCase()
-                )
-                && item.type.toUpperCase().includes(
-                  params.type.toUpperCase()
-                )
-              ))
-            })
-          )
-          .subscribe((items) => {
-            this.filteredItems$.next(items)
-          });
-      });
+    this.itemsDataService.getFilteredItems();
   }
 
   ngOnDestroy(): void {
-    this.filterSub.unsubscribe();
   }
 
 }
